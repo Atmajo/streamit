18af4b2713c1276d905c43bc3197fd8b2a15fefe
Atmajo <atmajoc@gmail.com>
2025-01-22 01:51:18 +0530

    Initialised docker in nextjs

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -16,13 +16,13 @@ export default function Home() {
           <li className="mb-2">
             Get started by editing{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
+              hello/index.tsx
             </code>
             .
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
-
+        
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
